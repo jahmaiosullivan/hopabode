b7f608b7ae1db8e62cc4f3817986afe238166a8c
Jahmai OSullivan <jahmai.osullivan@gmail.com>
2016-02-24 00:46:28 -0500

    styling

--- routes/apiroutes.ts
+++ routes/apiroutes.ts
@@ -11,18 +11,21 @@ class ApiRoutes implements Routing.IAppRoutes {
 
     getRoutes(app: express.Application, router: express.Router):any {
 
-        /*** Image Routes **/
+        /******************************************************************/
+        /*          Image Routes                                           */
+        /******************************************************************/
         router.post('/upload', multipartMiddleware, imagecontroller.post);
         router.options('/upload', imagecontroller.options);
         router.get('/upload', imagecontroller.get);
         router.get('/download/:identifier', imagecontroller.download);
 
-        /*** User Routes ***/
+        /******************************************************************/
+        /*          User Routes                                           */
+        /******************************************************************/
         router.route('/users').get(usercontroller.get)
                               .post(function (req, res) {})
                               .put(function (req, res) {})
                               .delete(function (req, res) {});
-
         router.get('/users/:id', usercontroller.get);
         router.get('/users/startswith/:startLetter', usercontroller.findStartingWith);
 
